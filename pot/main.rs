d8a5885b0bd0243e3bf763a94a3330c1c667d9f1
Joshua Patrick <jjpatric@ualberta.ca>
2021-01-22 09:42:11 -0700

    add get_input

--- pot/main.rs
+++ pot/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello World!");
+    let inp=get_input();
+    println!("You typed: {}",inp);
+}
+
+fn get_input() -> String{
+    use std::io::{stdin,stdout,Write};
+    let mut s=String::new();
+    let _=stdout().flush();
+    stdin().read_line(&mut s).expect("Did not enter a correct string");
+    if let Some('\n')=s.chars().next_back() {
+        s.pop();
+    }
+    if let Some('\r')=s.chars().next_back() {
+        s.pop();
+    }
+    s
 }
